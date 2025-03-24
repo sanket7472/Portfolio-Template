45f1c463e96cee27c444cdd4704f69f6dfaf26f4
Sanket Tamnar <158296853+sanket7472@users.noreply.github.com>
2025-03-24 10:40:21 +0530

    javascript functionalities added

--- script.js
+++ script.js
@@ -1,4 +1,17 @@
-let input = document.querySelector('input')
-let textarea = document.querySelector('textarea')
+let input = document.querySelector("input");
+let textarea = document.querySelector("textarea");
+let submitLink = document.querySelector("#submitLink");
+let description;
+let subject;
+input.addEventListener("input", (e) => {
+  subject = e.target.value;
+});
 
-input.addEventListener('input', (e))
+textarea.addEventListener("input", (e) => {
+  description = e.target.value;
+});
+
+submitLink.addEventListener("click", (e) => {
+e.preventDefault();
+submitLink.href = `mailto:sankettamnar2003@gmail.com?subject=${subject}&body=${description}`;
+});
